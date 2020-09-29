22bf7c19a0ebda74851d130cc9609448496bb100
Juan Jesus <castelock@gmail.com>
2020-09-29 22:32:22 +0200

    Adding the experiments

--- generating_input.py
+++ generating_input.py
@@ -2,7 +2,7 @@ import csv
 
 class Experiment:
 
-    def __init__(self, id, framework, model, optimizer, learning_rate, epsilon, epochs, loss_function, num_gestures, gestures, num_videos):
+    def __init__(self, id, framework, model, optimizer, learning_rate, epsilon, epochs, loss_function, num_gestures, gesture_names, num_videos):
 
         self.id = id
         self.framework = framework
@@ -13,8 +13,8 @@ class Experiment:
         self.epochs = epochs
         self.loss_function = loss_function
         self.num_gestures = num_gestures
-        # The type of this attribute is a list
-        self.gestures = gestures
+        # This attribute is a list
+        self.gesture_names = gesture_names
         self.num_videos = num_videos
 
     def getId(self):
@@ -44,8 +44,8 @@ class Experiment:
     def getNum_gestures(self):
         return self.num_gestures
 
-    def getGestures(self):
-        return self.gestures
+    def getGesture_names(self):
+        return self.gesture_names
 
     def getNum_videos(self):
         return self.num_videos
@@ -75,12 +75,30 @@ class Experiment:
 
 class Experiment_Metrics:
 
-    def __init__(self, precision, recall, f1_score, support):
+    def __init__(self, id, gestures):
         
+        self.id = id
+        # This attribute is a list
+        self.gestures = gestures
+
+    def getId(self):
+        return self.id
+    
+    def getGestures(self):
+        return self.gestures
+
+class Gesture:
+
+    def __init__(self, name, precision, recall, f1_score, support):
+        
+        self.name = name
         self.precision = precision
         self.recall = recall
         self.f1_score = f1_score
         self.support = support
+
+    def getName(self):
+        return self.name
 
     def getPrecision(self):
         return self.precision
@@ -106,13 +124,47 @@ filenameMetrics_path = input_path + filename_metrics
 
 print("Starting the input generation")
 
-exp1 = Experiment_Metrics("1.0", "1.0", "0.86", "98")
+# Adding the experiments
+gestures_3g = ["swipe_left", "thumb_up", "stop_sign"]
+gestures_5g = ["swipe_left", "thumb_up", "stop_sign", "swipe_down", "slide_2_fingers_right"]
+gestures_8g = ["swipe_left", "thumb_up", "stop_sign", "swipe_down", "slide_2_fingers_right", "slide_2_fingers_up", "zoom_in_with_2_fingers", "pull_hand_in"]
+gestures_10g = ["swipe_left", "thumb_up", "stop_sign", "swipe_down", "slide_2_fingers_right", "slide_2_fingers_up", "zoom_in_with_2_fingers", "pull_hand_in", "zoom_in_with_full_hand", "zoom_out_with_2_fingers"]
 
-gestures_list = ["swipe_left", "thumb_up", "stop_sign"]
+exp_1 = Experiment("1", "Keras", "ResNet50", "Adam", "0.001", "1.0", "100", "categorical_crossentropy", "3", gestures_3g, "10")
+exp_4 = Experiment("4", "Keras", "ResNet50", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_7 = Experiment("7", "Keras", "ResNet50V2", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_11 = Experiment("11", "Keras", "ResNet50", "Adam", "0.001", "1.0", "100", "mse", "3", gestures_3g, "10")
+exp_15 = Experiment("15", "Keras", "ResNet101V2", "Adam", "0.001", "1.0", "100", "mse", "3", gestures_3g, "10")
+exp_22 = Experiment("22", "Keras", "VGG16", "SGD", None, None, "100", "categorical_crossentropy", "3", gestures_3g, "10")
+exp_24 = Experiment("24", "Keras", "VGG16", "Adam", "0.001", "0.1", "100", "mse", "3", gestures_3g, "10")
+exp_27 = Experiment("27", "Keras", "VGG19", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_30 = Experiment("30", "Keras", "InceptionV3", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_33 = Experiment("33", "Keras", "InceptionResNetV2", "Adam", "0.001", "0.1", "100", "mse", "3", gestures_3g, "10")
+exp_34 = Experiment("34", "Keras", "MobileNetV2", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_37 = Experiment("37", "Keras", "DenseNet121", "Adam", "0.001", "0.1", "100", "mse", "3", gestures_3g, "10")
+exp_38 = Experiment("38", "Keras", "DenseNet169", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_41 = Experiment("41", "Keras", "DenseNet201", "Adam", "0.001", "0.1", "100", "mse", "3", gestures_3g, "10")
+exp_42 = Experiment("42", "Keras", "NasNetLarge", "SGD", None, None, "100", "mse", "3", gestures_3g, "10")
+exp_51 = Experiment("51", "Keras", "ResNet152", "Adam", "0.001", "0.1", "100", "mse", "5", gestures_5g, "10")
+exp_54 = Experiment("54", "Keras", "VGG16", "Adam", "0.001", "0.1", "100", "categorical_crossentropy", "5", gestures_5g, "10")
+exp_56 = Experiment("56", "Keras", "ResNet50", "SGD", None, None, "100", "mse", "5", gestures_5g, "10")
+exp_59 = Experiment("59", "Keras", "ResNet152", "Adam", "0.001", "0.1", "200", "mse", "5", gestures_5g, "10")
+exp_69 = Experiment("69", "Keras", "VGG16", "Adam", "0.001", "0.1", "100", "categorical_crossentropy", "8", gestures_8g, "10")
+exp_71 = Experiment("71", "Keras", "ResNet50", "SGD", None, None, "100", "mse", "8", gestures_8g, "10")
+exp_73 = Experiment("73", "Keras", "ResNet101", "Adam", "0.001", "0.1", "200", "mse", "8", gestures_8g, "10")
+exp_75 = Experiment("75", "Keras", "VGG16", "Adam", "0.001", "0.1", "200", "mse", "8", gestures_8g, "10")
+exp_76 = Experiment("76", "Keras", "VGG19", "Adam", "0.001", "0.1", "200", "mse", "8", gestures_8g, "10")
+exp_82 = Experiment("82", "Keras", "VGG16", "Adam", "0.001", "0.1", "100", "mse", "10", gestures_10g, "10")
+exp_85 = Experiment("85", "Keras", "VGG19", "Adam", "0.001", "0.1", "100", "categorical_crossentropy", "10", gestures_10g, "10")
+exp_88 = Experiment("88", "Keras", "ResNet152", "Adam", "0.001", "0.1", "200", "mse", "10", gestures_10g, "10")
+exp_89 = Experiment("89", "Keras", "VGG16", "Adam", "0.001", "0.1", "200", "mse", "10", gestures_10g, "10")
 
-exp_test = Experiment("1", "Keras", "ResNet50", "Adam", "0.001", "1.0", "100", "categorical_crossentropy", "3", gestures_list, "10")
-exp_test.print()
-print(exp_test.getGestures())
+# Adding the metrics
+swipe_left1 = Gesture("swipe_left", )
+
+
+
+
 
 # Testing write files
 exp_test.writeNewFile(filenameExp_path)
@@ -123,4 +175,7 @@ print(exp1.getF1_score())
 with open(filenameExp_path, newline='') as f:
     reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
     for row in reader:
-        print("New row:", row)
+        print("New row: ", row)
+        for item in row:
+            print("New item: ", item)
+        
