8efa53a00782c2f8be42572cbd804a6dc20b1514
Juan Jesus <castelock@gmail.com>
2020-12-07 22:10:05 +0100

    Adding the first rules to the fuzzy system

--- rank_fuzzy.py
+++ rank_fuzzy.py
@@ -1,6 +1,9 @@
 import fuzzylite as fl
 import GUI as gui_tk
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
+import statistics
 
 # TAKAGI-SUGENO-KANG
 
@@ -24,10 +27,36 @@ class TKS:
             maximum=1.000,
             lock_range=False,
             terms=[
-                fl.Gaussian("HIGH",x,4,0.8),
-                fl.Gaussian("MEDIUM",x,4,0.5),
-                fl.Gaussian("LOW", x,4,0.25) ]
-                
+                fl.Gaussian("HIGH",0.9,0.5),
+                fl.Gaussian("MEDIUM",0.6,0.5),
+                fl.Gaussian("LOW",0.35,0.5)
+                 ]                
+            ),
+        fl.InputVariable(
+            name="Recall",
+            description="",
+            enabled=True,
+            minimum=0.000,
+            maximum=1.000,
+            lock_range=False,
+            terms=[
+                fl.Gaussian("HIGH",0.9,0.5),
+                fl.Gaussian("MEDIUM",0.6,0.5),
+                fl.Gaussian("LOW",0.35,0.5)
+                 ]                
+            ),
+        fl.InputVariable(
+            name="F1_score",
+            description="",
+            enabled=True,
+            minimum=0.000,
+            maximum=1.000,
+            lock_range=False,
+            terms=[
+                fl.Gaussian("HIGH",0.9,0.5),
+                fl.Gaussian("MEDIUM",0.6,0.5),
+                fl.Gaussian("LOW",0.35,0.5)
+                 ]                
             )
         ]
 
@@ -45,9 +74,11 @@ class TKS:
             defuzzifier=fl.WeightedAverage("TakagiSugeno"),
             lock_previous=False,
             terms=[
-                fl.Constant("LOW", 0.250),
-                fl.Constant("MEDIUM", 0.500),
-                fl.Constant("HIGH", 0.750)]
+                fl.Constant("PERFECT", 1),
+                fl.Constant("GOOD", 0.9),
+                fl.Constant("MEDIOCRE", 0.6),
+                fl.Constant("BAD", 0.35),
+                ]
             )
         ]
 
@@ -58,14 +89,14 @@ class TKS:
                 name="Rules",
                 description="",
                 enabled=True,
-                conjunction=None,
-                disjunction=None,
+                conjunction=fl.Minimum(),
+                disjunction=fl.Maximum(),
                 implication=None,
                 activation=fl.Highest(),
                 rules=[
-                    fl.Rule.create("if Precision is HIGH then Result is HIGH", self.engine),
-                    fl.Rule.create("if Precision is MEDIUM then Result is MEDIUM", self.engine),
-                    fl.Rule.create("if Precision is LOW then Result is LOW", self.engine)
+                    fl.Rule.create("if Precision is HIGH and Recall is HIGH and F1_score is HIGH then Result is PERFECT", self.engine),
+                    fl.Rule.create("if F1_score is LOW then Result is BAD", self.engine),
+                    fl.Rule.create("if Precision is LOW then Result is BAD", self.engine)
                 ]
             )
         ]
@@ -194,14 +225,37 @@ engine.rule_blocks = [
 """window = gui_tk.Window("Fuzzy Expert System")
 window.mainloop()"""
 
+# PLOTS
+"""mean = 0
+
+standard_deviation = 1
+
+
+x_values = np.arange(-5, 5, 0.1)
+
+y_values = scipy.stats.norm(mean, standard_deviation)
+
+
+plt.plot(x_values, y_values.pdf(x_values))
+plt.show()"""
+
+"""x = np.arange(0,1,0.1)
+std_dev= statistics.stdev(x)
+y = scipy.stats.norm(0.5, 0.5)
+plt.plot(x,y.pdf(x))
+plt.show()"""
+
+
 # Creating the TKS Fuzzy System
 tks = TKS()
 tks.creating_input()
 tks.creating_output()
 tks.creating_fuzzy_rules()
-tks.engine.input_variable("Precision").value = 0.1
+tks.engine.input_variable("Precision").value = 0.5
 tks.engine.process()
 tks.engine.output_variable("Result").defuzzify()
+
+
 
 # print("Activation degree: ", tks.engine.output_variable("Power").fuzzy.activation_degree(tks.engine.output_variable("Power").term("HIGH")))
 #result = tks.engine.output_variable("Power").fuzzy_value()
@@ -220,15 +274,17 @@ for term in list_terms:
 
 list_rules = tks.engine.rule_block("Rules").rules
 
+tks.engine.rule_block("Rules").
+
 for rule in list_rules:
-    print("Trigerred: ", rule.triggered.__str__())
+    print("Trigerred: ", rule.triggered.__str__() + " " + rule.antecedent.text)
 
 threshold = 0.8
-print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("HIGH")).__str__())
-print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("MEDIUM")).__str__())
-print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("LOW")).__str__())
+print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("PERFECT")).__str__())
+print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("MEDIOCRE")).__str__())
+print( "Activation degree: ", tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("BAD")).__str__())
 
-if tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("HIGH")) >= threshold:
+if tks.engine.output_variable("Result").fuzzy.activation_degree(tks.engine.output_variable("Result").term("PERFECT")) >= threshold:
     print("Result is over threshold")
 else:
     print("Result is under threshold")
