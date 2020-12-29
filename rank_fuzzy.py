3a33c9dbac0b6acaf13600ece09256068989f07f
Juan Jesus <castelock@gmail.com>
2020-12-29 22:21:51 +0100

    Modifying the ranking code

--- rank_fuzzy.py
+++ rank_fuzzy.py
@@ -6,9 +6,146 @@ import scipy.stats
 import statistics
 import csv
 import generating_input as input_exp
+import sklearn.metrics
+from mlxtend.plotting import plot_confusion_matrix
+
+# METHODS
+def evaluate_predictions(list_perfect_pred, list_good_pred, list_mediocre_pred, list_low_pred):
+    list_perfect_true = {"24", "25", "26", "27", "28", "29", "44", "45", "47", "48", "52", "53", "54", "55", "60", "61", "62", "63", "69", "70", "75", "77", "78", "84", "85", "91", "92", "93", "94", "96", "98", "99", "103", "104"}
+    list_good_true = {"3", "4", "17", "19", "21", "22", "23", "46", "67", "68", "76", "82", "83", "89", "90", "97", "101", "102"}
+    list_mediocre_true = {"1", "2", "5", "6", "12", "13", "18", "20", "49", "51", "57", "58", "59", "64", "66", "72", "73", "74", "79", "80", "81", "86", "87", "88", "95", "100"}
+    list_low_true = {"7", "8", "9", "10", "11", "14", "15", "16", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "50", "56", "65", "71"}
+
+    # Right answers for each case
+    pred_pp = 0
+    pred_pg = 0
+    pred_pm = 0
+    pred_pl = 0
+
+    pred_gg = 0
+    pred_gp = 0
+    pred_gm = 0
+    pred_gl = 0
+
+    pred_mm = 0
+    pred_mp = 0
+    pred_mg = 0
+    pred_ml = 0
+
+    pred_ll = 0
+    pred_lp = 0
+    pred_lg = 0
+    pred_lm = 0
+
+    # Accuracy rate 
+    acc_rate_perfect = 0
+    acc_rate_good = 0
+    acc_rate_mediocre = 0
+    acc_rate_low = 0
+
+
+    # Counting the right answers for perfect cases
+    for element in list_perfect_pred:
+        if element in list_perfect_true:
+            pred_pp+=1
+        elif element in list_good_true:
+            pred_pg+=1
+        elif element in list_mediocre_true:
+            pred_pm+=1
+        elif element in list_low_true:
+            pred_pl+=1
+
+    # Counting the right answers for good cases
+    for element in list_good_pred:
+        if element in list_good_true:
+            pred_gg+=1
+        elif element in list_perfect_true:
+            pred_gp+=1
+        elif element in list_mediocre_true:
+            pred_gm+=1
+        elif element in list_low_true:
+            pred_gl+=1
+    
+    # Counting the right answers for mediocrre cases
+    for element in list_mediocre_pred:
+        if element in list_mediocre_true:
+            pred_mm+=1
+        elif element in list_perfect_true:
+            pred_mp+=1
+        elif element in list_good_true:
+            pred_mg+=1
+        elif element in list_low_true:
+            pred_ml+=1
+
+    # Counting the right answers for low cases
+    for element in list_low_pred:
+        if element in list_low_true:
+            pred_ll+=1
+        elif element in list_perfect_true:
+            pred_lp+=1
+        elif element in list_good_true:
+            pred_lg+=1
+        elif element in list_mediocre_true:
+            pred_lm+=1
+
+    # Calculating the accurate rate for each case
+    # acc = (TP + TN) / (TP + TN + FP + FN) 
+    """acc_rate_perfect = right_perfect / len(list_perfect_true)
+    acc_rate_good = right_good / len(list_good_true)
+    acc_rate_mediocre = right_mediocre / len(list_mediocre_true)
+    acc_rate_low = right_low / len(list_low_true)
+
+    acc_rate = sklearn.metrics.accuracy_score(list_perfect_true, list_perfect_pred)"""
+
+    tp_perfect = pred_pp
+    tn_perfect = pred_gg+pred_gm+pred_gl+pred_mg+pred_mm+pred_ml+pred_lg+pred_lm+pred_ll
+    fp_perfect = pred_pg+pred_pm+pred_pl
+    fn_perfect = pred_gp+pred_mp+pred_lp
+
+    acc_rate_perfect = (tp_perfect + tn_perfect) / (tp_perfect + tn_perfect + fp_perfect + fn_perfect)
+    precision_perfect = tp_perfect/(tp_perfect+fp_perfect)
+
+    tp_good = pred_gg
+    tn_good = pred_pp+pred_pm+pred_pl+pred_mp+pred_mm+pred_ml+pred_lp+pred_lm+pred_ll
+    fp_good = pred_gp+pred_gm+pred_gl
+    fn_good = pred_pg+pred_mg+pred_lg
+
+    acc_rate_good = (tp_good + tn_good) / (tp_good + tn_good + fp_good + fn_good)
+    precision_good = tp_good/(tp_good+fp_good)
+
+    tp_mediocre = pred_mm
+    tn_mediocre = pred_pp+pred_pg+pred_pl+pred_gp+pred_gg+pred_gl+pred_lp+pred_lg+pred_ll
+    fp_mediocre = pred_mp+pred_mg+pred_ml
+    fn_mediocre = pred_pm+pred_gm+pred_gl
+
+    acc_rate_mediocre = (tp_mediocre + tn_mediocre) / (tp_mediocre + tn_mediocre + fp_mediocre + fn_mediocre)
+    precision_mediocre = tp_mediocre/(tp_mediocre+fp_mediocre)
+    
+
+    tp_low = pred_ll
+    tn_low = pred_pp+pred_pg+pred_pm+pred_gp+pred_gg+pred_gm+pred_mp+pred_mg+pred_mm
+    fp_low = pred_lp+pred_lg+pred_lm
+    fn_low = pred_pl+pred_gl+pred_ml
+
+    acc_rate_low = (tp_low + tn_low) / (tp_low + tn_low + fp_low + fn_low)
+    precision_low = tp_low/(tp_low+fp_low)
+
+
+    confusion_matrix = np.array([[pred_pp,pred_pg,pred_pm,pred_pl], [pred_gp,pred_gg,pred_gm,pred_gl], [pred_mp,pred_mg,pred_mm,pred_ml], [pred_lp,pred_lg,pred_lm,pred_ll]])
+
+    class_names = ["Perfect", "Good", "Mediocre", "Low"]
+
+    fig, ax = plot_confusion_matrix(conf_mat=confusion_matrix,
+                                colorbar=True,
+                                show_absolute=True,
+                                show_normed=False,
+                                class_names=class_names)
+    plt.show()
+
+    return acc_rate_perfect, acc_rate_good, acc_rate_mediocre, acc_rate_low
+
 
 # TAKAGI-SUGENO-KANG
-
 
 class TKS:
 
@@ -99,8 +236,7 @@ class TKS:
                 rules=[
                     fl.Rule.create("if Precision is HIGH and Recall is HIGH and F1_score is HIGH then Result is PERFECT", self.engine),
                     fl.Rule.create("if Precision is HIGH and Recall is MEDIUM and F1_score is HIGH then Result is GOOD", self.engine),
-                    fl.Rule.create("if Precision is MEDIUM and Recall is MEDIUM and F1_score is HIGH then Result is GOOD", self.engine),
-                    fl.Rule.create("if Precision is HIGH and Recall is MEDIUM and F1_score is HIGH then Result is GOOD", self.engine),
+                    fl.Rule.create("if Precision is MEDIUM and Recall is HIGH and F1_score is HIGH then Result is GOOD", self.engine),                    
                     fl.Rule.create("if Precision is MEDIUM and Recall is HIGH and F1_score is MEDIUM then Result is MEDIOCRE", self.engine),
                     fl.Rule.create("if Precision is MEDIUM and Recall is MEDIUM and F1_score is MEDIUM then Result is MEDIOCRE", self.engine),
                     fl.Rule.create("if Precision is HIGH and Recall is LOW and F1_score is MEDIUM then Result is MEDIOCRE", self.engine),
@@ -344,11 +480,6 @@ for metric in list_metrics:
         print("ERROR: The output variable is wrong.")
 
 
-
-#tks.engine.output_variable("Result").defuzzify()
-
-
-
 # print("Activation degree: ", tks.engine.output_variable("Power").fuzzy.activation_degree(tks.engine.output_variable("Power").term("HIGH")))
 #result = tks.engine.output_variable("Power").fuzzy_value()
 #print("Result: ", result)
@@ -356,8 +487,6 @@ for metric in list_metrics:
 print("Rules: ", tks.engine.rule_block("Rules").rules[0].weight.__str__())
 print("Rules: ", tks.engine.rule_block("Rules").rules[1].weight.__str__())
 print("Rules: ", tks.engine.rule_block("Rules").rules[2].weight.__str__())
-
-#print("Value output low:", tks.engine.output_variable("Result").terms.
 
 list_terms = tks.engine.output_variable("Result").terms
 for term in list_terms:
@@ -387,6 +516,7 @@ else:
 # Create dictionary for the results
 dic_result={"PERFECT": list_perfect, "GOOD": list_good, "MEDIOCRE": list_mediocre, "LOW": list_low}
 
-
+# Evaluate the results
+evaluate_predictions(dic_result["PERFECT"], dic_result["GOOD"], dic_result["MEDIOCRE"], dic_result["LOW"])
 
 print("PROCESS FINISHED")
